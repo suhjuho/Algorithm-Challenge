ed9ad4cdec8fcf2c1d7c625e93165e8d0d117f4e
seongjun hong <133403759+suhjuho@users.noreply.github.com>
2024-03-31 22:53:55 +0900

    Time: 68 ms (14.56%), Space: 51.6 MB (20.44%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -6,29 +6,29 @@
 const addBinary = function(a, b) {
     let result = "";
 
-    const aa = a.split("").reverse().map(Number);
-    const bb = b.split("").reverse().map(Number);
-    const maxLength = Math.max(aa.length, bb.length);
+    const reverseA = a.split("").reverse().map(Number);
+    const reverseB = b.split("").reverse().map(Number);
+    const maxLength = Math.max(reverseA.length, reverseB.length);
 
     let carry = 0;
 
     for (let i = 0; i < maxLength; i++) {
-        if (!aa[i]) {
-            aa[i] = 0;
+        if (!reverseA[i]) {
+            reverseA[i] = 0;
         }
 
-        if (!bb[i]) {
-            bb[i] = 0;
+        if (!reverseB[i]) {
+            reverseB[i] = 0;
         }
 
-        if (aa[i] + bb[i] + carry === 3) {
+        if (reverseA[i] + reverseB[i] + carry === 3) {
             result += "1";
             carry = 1;
-        } else if (aa[i] + bb[i] + carry === 2) {
+        } else if (reverseA[i] + reverseB[i] + carry === 2) {
             result += "0";
             carry = 1;
         } else {
-            result += aa[i] +bb[i] + carry;
+            result += reverseA[i] +reverseB[i] + carry;
             carry = 0;
         }
     }
