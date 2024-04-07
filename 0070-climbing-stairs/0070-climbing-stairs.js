4171135e840da568516901e806859d3064325677
seongjun hong <133403759+suhjuho@users.noreply.github.com>
2024-04-07 15:32:38 +0900

    Time: 54 ms (36.13%), Space: 48.1 MB (95.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -10,7 +10,7 @@ const climbStairs = function(n) {
     const result = [1, 2];
 
     for (let i = 2; i < n; i += 1){
-        result.push(result[i -2] + result[i - 1]);
+        result.push(result[i - 2] + result[i - 1]);
     }
 
     return result[n - 1];
