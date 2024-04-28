d2c119899f78bd9993fc765133b1528c24d37192
seongjun hong <133403759+suhjuho@users.noreply.github.com>
2024-04-28 14:28:17 +0900

    Time: 60 ms (58.62%), Space: 51.9 MB (42.28%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function(root) {
+    if (!root) {
+        return 0;
+    }
+
+    let result = 0;
+
+    function checkDepth(node, depth) {
+        if (node) {
+            result = Math.max(result, depth);
+        }
+
+        if (node.left) {
+            checkDepth(node.left, depth + 1);
+        }
+
+        if (node.right) {
+            checkDepth(node.right, depth + 1);
+        }
+    }
+
+    checkDepth(root, 1);
+
+    return result;
+};
