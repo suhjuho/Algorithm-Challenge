1692bca76695a5dc4e8f376c1512a69b7b5ae4d5
seongjun hong <133403759+suhjuho@users.noreply.github.com>
2024-05-03 02:58:35 +0900

    Time: 185 ms (21.12%), Space: 75.1 MB (43.63%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+const sortList = function(head) {
+    if (!head) {
+        return head;
+    }
+
+    const numbers = [];
+    let node = head;
+
+    while (node) {
+        numbers.push(node.val);
+
+        node = node.next;
+    }
+
+    console.log(numbers.sort((a, b) => a - b));
+
+    let tail;
+    let newHead;
+
+    for (let i = numbers.length - 1; i >= 0; i -= 1) {
+        newHead = new ListNode(numbers[i], tail);
+
+        tail = newHead;
+    }
+
+    return newHead;
+};
