768a4906164fb2993075246709311e17db51839c
seongjun hong <133403759+suhjuho@users.noreply.github.com>
2024-05-14 02:22:13 +0900

    Time: 98 ms (8.95%), Space: 59.2 MB (41.34%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function(nums, k) {
+    const len = nums.length;
+    const step = k % len;
+    const visited = Array(len).fill(false);
+
+    let currentIndex = 0;
+    let currentValue = nums[currentIndex];
+    let count = 0;
+
+    while (count < len) {
+        const newIndex = (currentIndex + step) % len;
+        const nextValue = nums[newIndex];
+
+        if (visited[newIndex]) {
+            currentIndex = (currentIndex + 1) % len;
+            currentValue = nums[currentIndex];
+            continue;
+        }
+
+        nums[newIndex] = currentValue;  
+        currentIndex = newIndex;
+        currentValue = nextValue;
+        count += 1;
+        visited[newIndex] = true;
+    }
+};
