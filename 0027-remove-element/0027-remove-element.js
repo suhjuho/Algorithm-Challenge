4068db0bc94246a25147cb260808d378507cfb53
seongjun hong <133403759+suhjuho@users.noreply.github.com>
2024-05-29 15:20:20 +0900

    Time: 50 ms (74.62%), Space: 49.1 MB (32.51%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[index] = nums[i];
+            index += 1;
+        }
+    }
+
+    return index;
+};
